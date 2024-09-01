ced2e059db92e7a36fc907656a2c63dff326035c
Mahdi_204 <abaalamen1@gmail.com>
2024-09-01 10:50:25 +0300

    تصحيح كم خطأ وتحسين دالة إنشاء قاعدة البيانات.

--- Datebase/Datebase/DatabaseManager.cs
+++ Datebase/Datebase/DatabaseManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,28 +15,50 @@ namespace Datebase
         /// <summary>
         /// the database you are managing.
         /// </summary>
-        public Database Database { get; set; }
-        public DatabaseManager(string name) { Database = new Database(name); }
-
+        /// 
+        public string Path { get; set; }
+        public DatabaseManager(string name) { Path = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.Parent.FullName + "\\" + name }
+        public DatabaseManager(string name, string path) : this(name) { Path = path; }
         static void Creatdirs(string path, string name)
         {
-            Directory.CreateDirectory(path + "\\" + name);
-            Directory.CreateDirectory(path + "\\" + name + "\\Users");
-            Directory.CreateDirectory(path + "\\" + name + "\\Users\\Costomers");
-            Directory.CreateDirectory(path + "\\" + name + "\\Users\\Seller");
-            Directory.CreateDirectory(path + "\\" + name + "\\Products");
-            Directory.CreateDirectory(path + "\\" + name + "\\Products\\Photos");
-            Directory.CreateDirectory(path + "\\" + name + "\\Products\\Coppons");
+            string Database = path + "\\" + name;
+            string[] dirs = { "", "\\Users", "\\Costomers", "\\Seller", "\\Products", "\\Comments" };
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                string s = Database;
+                if (i < 1)
+                {
+                    s += dirs[i];
+                    Directory.CreateDirectory(s);
+                    using (FileStream fs = File.Create(s + "\\counter.txt")) { }
+                    using (StreamWriter streamWriter = new StreamWriter(s + "\\counter.txt")) { streamWriter.Write("0"); }
+                }
+                else Directory.CreateDirectory(s);
+            }
         }
-        public static Database CreatDateBase(string path, string name)
+        /// <summary>
+        /// هي الميثود عنا ألها نسختين, وحدة مع Id والتانية بدونو، هو مالو طعمة فعليا بس حط بالخرج.
+        /// هي الدالة مهمتها تساوي قاعدة البيانات بكل الملفات والمجلدات يلي راح تلزم تكون بقلبها من شان نشتغل عليها بعدين.
+        /// قبل ما نبلش شغل بالقواعد لازم ننشئ قاهدة ونربطها بالبرنامج من شان يرجعلها بعدين.
+        /// </summary>
+        /// <param name="path">المسار يلي بدك يكون فيه مجلد القاعدة</param>
+        /// <param name="name">اسم القاعدة</param>
+        /// <returns>ترجع مسار القاعدة من شان تستعملو بإنشاء كائن القاعدة.</returns>
+        public static string CreatDateBase(string path, string name)
         {
             Creatdirs(path, name);
-            return new Database(path, name);
+            return path + "\\" + name;
         }
-        public static Database CreatDateBase(string path, string name, string id)
+        /// <summary>
+        /// نفس أختها بس بتنشئ القاعدة بقلب المسار اللي عم يتم تنفيذ المشروع فيه.
+        /// </summary>
+        /// <param name="name">اسم القاعدة</param>
+        /// <returns></returns>
+        public static string CreatDateBase(string name)
         {
+            string path = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).FullName;
             Creatdirs(path, name);
-            return new Database(path, name, id);
+            return path + "\\" + name;
         }
     }
 }
--- Datebase/Datebase/obj/Debug/net8.0/Datebase.AssemblyInfo.cs
+++ Datebase/Datebase/obj/Debug/net8.0/Datebase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datebase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f16e1223fcbe5ae392802436187592df05413b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datebase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datebase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
